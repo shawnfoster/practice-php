77324669dcf3617f1f369e3a44992236785bedfd
Shawn Foster <scenefoster@gmail.com>
2017-03-08 14:13:31 -0600

    practicing basics

---
+++ practice.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    // Create a Variable
+    <?php
+        $myName = "Shawn";
+        // no quotes around #'s
+        $myAge = 40;
+          echo $myName;
+          echo $myAge;
+          // shows Shawn40
+        ?>
+  </body>
+</html>
